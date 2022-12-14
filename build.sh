6072ae0b8d8fd558b81f566cdcd15f1aab03e382
Crony Akatsuki <cronyakatsuki@tutanota.com>
2022-12-14 18:48:37 +0100

    Update startpage.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-rsync -aup -e 'ssh -p 8044' --delete public/* root@cronyakatsuki.xyz:/var/www/startpage
+rsync -auP --delete public/* root@cronyakatsuki.xyz:/var/www/startpage
 git add .
 git commit -m "Update startpage."
